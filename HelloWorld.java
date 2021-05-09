f8c23fe49402a7ea22ca5b4c2642055126316e11
Irarf <64776637+Irarf@users.noreply.github.com>
2021-05-09 23:11:11 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -33,8 +33,8 @@ public class HelloWorld {
 
         storageCnf.setStoragePath("/root/lab2/cache");
 
-       // cfg.setDataStorageConfiguration(storageCnf);
-     //   // Запуск узла, как клиента.
+        // cfg.setDataStorageConfiguration(storageCnf);
+        //   // Запуск узла, как клиента.
         cfg.setClientMode(true);
 
         // Classes of custom Java logic will be transferred over the wire from this app.
@@ -64,7 +64,7 @@ public class HelloWorld {
 
 
             while (line !=null){
-                String[] words = line.split(",");
+                String[] words = line.split(",",0);
                 //String stroka =words[0] + "," + words[1] + "," + words[2] + ","+ words[3];
                 cache.put(i, new Air(Integer.parseInt(words[0]),Integer.parseInt(words[1]),words[2],words[3]));
                 //cache.put(i, new Air(1,2,"words[2]","words[3]"));
@@ -79,14 +79,14 @@ public class HelloWorld {
             e.printStackTrace();
         }
 
-      //  for (int i =1; i < 30;i++){
-     //       Air airtest = cache.get(i);
+          //for (int i =1; i < 30;i++){
 
-           // Air airtest = ge
-           // System.out.println(">>> cache'" + airtest);
-          //  int test1 = airtest.getTimestamp();
-         //   System.out.println(">>> Id'" + test1);
-     //   }
+
+        // Air airtest = ge
+        // System.out.println(">>> cache'" + cache.get(1));
+        //  int test1 = airtest.getTimestamp();
+        //   System.out.println(">>> Id'" + test1);
+        //   }
         try {
             int i = 1;
             File file1 = new File("/root/lab2/ttttt.txt");
@@ -110,6 +110,7 @@ public class HelloWorld {
             e.printStackTrace();
         }
         countItog = countAir + "," + countBible;
+
         /*
         ArrayList<String> lines = new ArrayList<>();
         try(Scanner scan = new Scanner((new File("/root/lab2/input")))) {
@@ -128,10 +129,10 @@ public class HelloWorld {
         // Execute the task on the cluster and wait for its completion.
         int cnt = compute.execute(CharacterCountTask.class, countItog);
         // System.out.println(">>> Total number of characters in the phrase is '" + cnt + "'.");
-      //  System.out.println(">> Created the cache and add the values.");
+        //  System.out.println(">> Created the cache and add the values.");
 
         // Executing custom Java compute task on server nodes.
-       // ignite.compute(ignite.cluster().forServers()).broadcast(new RemoteTask());
+        // ignite.compute(ignite.cluster().forServers()).broadcast(new RemoteTask());
 
         System.out.println(">> Compute task is executed, check for output on the server nodes.");
 
@@ -140,6 +141,133 @@ public class HelloWorld {
         cacheBible.destroy();
         ignite.close();
     }
+
+
+
+    public static class CharacterCountTask extends ComputeTaskSplitAdapter<String, Integer> {
+        // 1. Splits the received string into words
+        // 2. Creates a child job for each word
+        // 3. Sends the jobs to other nodes for processing.
+        @IgniteInstanceResource
+        Ignite ignite;
+
+        @Override
+        public List<ComputeJob> split(int gridSize, String arg) {
+            String[] count = arg.split(",");
+            IgniteCache<Integer, Air> cache = ignite.cache("myCache");
+            IgniteCache<Integer, Bible> cacheBible = ignite.getOrCreateCache("byble");
+/*
+                ArrayList<String> lines = new ArrayList<>();
+                try(Scanner scan = new Scanner(new File("/root/lab2/ttttt.txt"))){
+                    while(scan.hasNextLine()){
+                        lines.add(scan.nextLine());
+                    }
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+
+                String[] array = lin    es.toArray(new String[0]);
+*/
+            //for (int i =1; i < 30;i++)
+            //System.out.println(">>> Printing '" + cacheBible.get(i) + "'   ");
+            List<ComputeJob> jobs = new ArrayList<>();
+
+            for (int i =1; i < Integer.parseInt(count[0]);i++) {
+                Air cacheStrokaAir = cache.get(i);
+                //System.out.println(">>>cacheDo- " + cacheStrokaAir);
+
+                //String stroka1 = cacheBible.get(i);
+                //String[] words = stroka.split(",");
+                // String[] words1 = stroka1.split(",");
+                int finalI = i;
+                jobs.add(new ComputeJobAdapter() {
+                             @Override
+                             public Object execute() {
+                                 //System.out.println(">>> Printing Id = " + finalI + " words = " + words[1] + " bible = " );
+/*
+                            // Округляем часы
+                            int intReturn = Integer.parseInt(words[1]);
+                            intReturn = intReturn - (intReturn % 3600);
+                            words[1] = Long.toString(intReturn);
+                            System.out.println(">>> Printing modif'" + words[1] + "' on from compute job.");
+                            //Заменяем аэропорты на страны
+*/
+
+                                 Bible cacheStrokaBible;
+                                 String airportOtByble;
+
+                                 String airportAirIn = cacheStrokaAir.getAirIn();
+                                 String airportAirOut = cacheStrokaAir.getAirOut();
+
+                                 String countryVmestoAirIn = null;
+                                 String countryVmestoAirOut = null;
+                                 //System.out.println(">>> airportAirIn - " + airportAirIn + "; airportAirOut - " + airportAirOut);
+                                 //System.out.println(">>>airport - " + airportAirOut + "; country - " + countryVmestoAirOut);
+                                 for (int y =1; y < Integer.parseInt(count[1]);y++){
+                                     boolean in = true;
+                                     boolean out = true;
+                                     cacheStrokaBible = cacheBible.get(y);
+                                     airportOtByble = cacheStrokaBible.getAirport();
+                                     //System.out.println(">>> airportAirIn - " + airportAirOut + "; airportOtByble - " + airportOtByble);
+                                     if (airportOtByble.equals(airportAirOut) && out){
+                                         countryVmestoAirOut = cacheStrokaBible.getCountry();
+                                         out = false;
+                                     }
+                                     if (airportOtByble.equals(airportAirIn) && in){
+                                         countryVmestoAirIn = cacheStrokaBible.getCountry();
+                                         in = false;
+                                     }
+                                     if (!out && !in)
+                                         break;
+
+                                 }
+/*
+                                 for (int j =1; j < Integer.parseInt(count[1]);j++){
+                                     cacheStrokaBible = cacheBible.get(j);
+                                     airportOtByble = cacheStrokaBible.getAirport();
+                                     //System.out.println(">>> airportAirOut - " + airportAirOut + "; airportOtByble - " + airportOtByble);
+                                     if (airportOtByble.equals(airportAirOut)){
+                                         countryVmestoAirOut = cacheStrokaBible.getCountry();
+                                         System.out.println(">>> airportAirOut - " + airportAirOut + "; airportOtByble - " + airportOtByble);
+                                         break;
+                                     }
+                                 }
+*/
+                                 int time = cacheStrokaAir.getTimestamp();
+                                 time = time - (time % 3600);
+                                 cacheStrokaAir.setAirIn(countryVmestoAirIn);
+                                 cacheStrokaAir.setAirOut(countryVmestoAirOut);
+                                 cacheStrokaAir.setTimestamp(time);
+                                /* cache.put(finalI,new Air((cacheStrokaAir.getId()),
+                                         (cacheStrokaAir.setTimestamp(time)),
+                                         (cacheStrokaAir.setAirIn(countryVmestoAirIn)),
+                                         (cacheStrokaAir.setAirOut(countryVmestoAirOut))));*/
+                                // System.out.println(">>>airportIn - " + airportAirIn + "; countryIn - " + countryVmestoAirIn);
+                                // System.out.println(">>>airportOut - " + airportAirOut + "; countryOut - " + countryVmestoAirOut);
+                                 System.out.println(">>>cache - " + cacheStrokaAir);
+                                 System.out.println("-------------------------------------------");
+                                 return time;
+                             }
+                         }
+                );
+            }
+
+            return jobs;
+        }
+
+        @Override
+        public Integer reduce(List<ComputeJobResult> results) {
+            int sum = 0;
+
+            for (ComputeJobResult res : results) {
+                sum += res.<Integer>getData();
+                System.out.println(">>> results on from compute job. res.<Integer>getData() " + res.<Integer>getData());
+            }
+
+            return sum;
+        }
+    }
+
 
     public static class Air{
         private int id;
@@ -178,7 +306,7 @@ public class HelloWorld {
             this.timestamp = timestamp;
         }
         @Override public String toString() {
-            return "Air [id=" + id +" timestamp="+ timestamp +" airIn"+airIn+" airOut"+airOut+"]";
+            return "Air [id=" + id +" timestamp="+ timestamp +" airIn="+airIn+" airOut="+airOut+"]";
         }
     }
 
@@ -191,7 +319,10 @@ public class HelloWorld {
             this.airport = airport;
             this.country = country;
         }
-        public String getAirport(){return airport;}
+        public String getAirport(){
+            return airport;
+        }
+
         public void setAirport(String airport){
             this.airport = airport;
         }
@@ -204,137 +335,6 @@ public class HelloWorld {
 
         @Override public String toString() {
             return "Bible [airport=" + airport +" country="+ country +"]";
-        }
-    }
-
-        public static class CharacterCountTask extends ComputeTaskSplitAdapter<String, Integer> {
-            // 1. Splits the received string into words
-            // 2. Creates a child job for each word
-            // 3. Sends the jobs to other nodes for processing.
-            @IgniteInstanceResource
-            Ignite ignite;
-
-            @Override
-            public List<ComputeJob> split(int gridSize, String arg) {
-                String[] count = arg.split(",");
-                IgniteCache<Integer, Air> cache = ignite.cache("myCache");
-                IgniteCache<Integer, Bible> cacheBible = ignite.getOrCreateCache("byble");
-/*
-                ArrayList<String> lines = new ArrayList<>();
-                try(Scanner scan = new Scanner(new File("/root/lab2/ttttt.txt"))){
-                    while(scan.hasNextLine()){
-                        lines.add(scan.nextLine());
-                    }
-                }catch (FileNotFoundException e){
-                    e.printStackTrace();
-                }
-
-                String[] array = lin    es.toArray(new String[0]);
-*/
-                //for (int i =1; i < 30;i++)
-                //System.out.println(">>> Printing '" + cacheBible.get(i) + "'   ");
-                List<ComputeJob> jobs = new ArrayList<>();
-
-                for (int i =1; i < Integer.parseInt(count[0]);i++) {
-                    Air cacheStrokaAir = cache.get(i);
-
-                    //String stroka1 = cacheBible.get(i);
-                    //String[] words = stroka.split(",");
-                   // String[] words1 = stroka1.split(",");
-                    int finalI = i;
-                    jobs.add(new ComputeJobAdapter() {
-                        @Override
-                        public Object execute() {
-                            //System.out.println(">>> Printing Id = " + finalI + " words = " + words[1] + " bible = " );
-/*
-                            // Округляем часы
-                            int intReturn = Integer.parseInt(words[1]);
-                            intReturn = intReturn - (intReturn % 3600);
-                            words[1] = Long.toString(intReturn);
-                            System.out.println(">>> Printing modif'" + words[1] + "' on from compute job.");
-                            //Заменяем аэропорты на страны
-*/
-
-                            Bible cacheStrokaBible;
-                            String airportOtByble;
-
-                            String airportAir = cacheStrokaAir.getAirIn();
-                            String countryVmestoAir = null;
-
-
-                            for (int y =1; y < Integer.parseInt(count[1]);y++){
-                                cacheStrokaBible = cacheBible.get(y);
-                                airportOtByble = cacheStrokaBible.getAirport();
-                                if (airportOtByble.equals(airportAir)){
-                                    countryVmestoAir = cacheStrokaBible.getCountry();
-                                    break;
-                                }
-                            }
-
-                            System.out.println(">>>airport - " + airportAir + "; country - " + countryVmestoAir);
-
-                            System.out.println("-------------------------------------------");
-                            return finalI;
-                        }
-                    }
-                    );
-                }
-
-                return jobs;
-            }
-
-            @Override
-            public Integer reduce(List<ComputeJobResult> results) {
-                int sum = 0;
-
-                for (ComputeJobResult res : results) {
-                    sum += res.<Integer>getData();
-                    System.out.println(">>> results on from compute job. res.<Integer>getData() " + res.<Integer>getData());
-                }
-
-                return sum;
-            }
-        }
-    /**
-     * A compute tasks that prints out a node ID and some details about its OS and JRE.
-     * Plus, the code shows how to access data stored in a cache from the compute task.
-     */
-    private static class RemoteTask implements IgniteRunnable {
-        @IgniteInstanceResource
-        Ignite ignite;
-
-
-
-        @Override public void run() {
-            try {
-                File file = new File("/root/lab2/ttttt.txt");
-                FileReader fr = new FileReader(file);
-                BufferedReader reader = new BufferedReader(fr);
-                String line = reader.readLine();
-
-                while (line !=null){
-                    String[] words = line.split("\t");
-
-                    line = reader.readLine();
-
-                }
-
-            }catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println(">> Executing the compute task");
-
-            System.out.println(
-                    "   Node ID: " + ignite.cluster().localNode().id() + "\n" +
-                            "   OS: " + System.getProperty("os.name") +
-                            "   JRE: " + System.getProperty("java.runtime.name"));
-
-            IgniteCache<Integer, String> cache = ignite.cache("myCache");
-            for (int i =1; i < 30;i++)
-            System.out.println(">> " + cache.get(i));
         }
     }
 
