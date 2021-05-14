8db4d94b11dbf62fb1c4cbdfb66c4b28c3539b1e
Irarf <64776637+Irarf@users.noreply.github.com>
2021-05-15 01:22:25 +0300

    Update test1.java

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -4,17 +4,7 @@ import hw2.exmaple.org.HW2;
 
 
 public class test1 {
-    private HW2 hw2;
 
-    @Before
-    public void initTest() {
-        hw2 = new HW2();
-    }
-
-    @Test
-    public void tetsArgs() throws Exception {
-        assertNotNull(hw2);
-    }
 
 
 }
